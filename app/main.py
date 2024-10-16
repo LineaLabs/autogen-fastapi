66f8d94587d6317f0567161e825054f69bd46c0a
关习习 <3494084267@qq.com>
2024-10-16 21:57:43 +0800

    在main里添加了请求头中的key检验 在app文件夹目录下新增了agents的json格式文件 在autogen_agents.py里添加了agents_configs的初始化函数 在配置文件docker-compose中的environment中添加了AUTH_KEYS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI
-from fastapi import HTTPException
+import os
+from fastapi import FastAPI, Depends, HTTPException, Request, Response
 from starlette.responses import RedirectResponse
 
 from autogen_server import serve_autogen
@@ -8,6 +8,24 @@ from data_model import Input, ModelInformation
 from autogen_agents import models
 
 load_dotenv()
+
+# 读取配置文件中的keys
+AUTH_KEYS: os.getenv("AUTH_KEYS").split(",")
+
+# 验证请求头中是否有正确的api_key
+def authorization(req: Request):
+    if(not req.headers.get("Authorization")):
+        raise HTTPException(
+                status_code=401,
+                detail="Unauthorized"
+            )
+    token = req.headers["Authorization"].replace("Bearer ", "")
+    if token not in AUTH_KEYS:
+        raise HTTPException(
+                status_code=401,
+                detail="Unauthorized"
+            )
+    return True
 
 base = "/autogen/"
 prefix = base + "api/v1"
@@ -35,7 +53,7 @@ async def get_models():
 
 
 @app.post(prefix + "/chat/completions")
-async def route_query(model_input: Input):
+async def route_query(model_input: Input, authorized: bool = Depends(authorization)):
     # model_services = {
     #     model_info.name: serve_autogen,
     # }
